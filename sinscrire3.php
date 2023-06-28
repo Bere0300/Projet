d30f7e071c9901a531db99b2c0442738233f447d
Bérénice Jarry Dev <127194470+Bere0300@users.noreply.github.com>
2023-06-28 10:38:32 +0200

    Add files via upload

---
+++ sinscrire3.php
@@ -0,0 +1,13 @@
+<?php
+
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\Extension\DebugExtension;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader= new FilesystemLoader(__DIR__. '/templates');
+$twig= new Environment($loader, ['debug'=>true]);
+$twig ->addExtension(new DebugExtension);
+echo $twig->render('sinscrire3.html');
